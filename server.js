b875840a90865b5bc0483e2a9fa789668c35dd84
Your New Name <lavanyakattunga2003@gmail.com>
2025-03-09 12:51:38 +0530

    Fix: CROS

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const recipeRoutes = require("./routes/recipeRoutes");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true,
+}));
 
 connectDB();
 
